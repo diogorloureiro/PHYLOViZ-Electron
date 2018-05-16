a0dc0344511a87771aaffa9f1fb5e15441a4b2df
JupitersMight <leonardo_duarte_rodrigues@hotmail.com>
2018-05-17 00:32:21 +0100

    rewrote grapetree.

--- web/public/javascripts/grapeTree.js
+++ web/public/javascripts/grapeTree.js
@@ -32,8 +32,7 @@ function buildDirectedGraph(parent, node, graph, new_graph) {
     node = {
         id: node.id,
         children: edges.map(edge => ({ id: edge.target })),
-        circle_radius: 1, // to be changed into an actual radius
-        parent,
+        size: 5, // to be changed into an actual radius
         distance: edge ? edge.distance : 0 // root node doesn't have parent
     }
     // Recursive call and replace the dummy children with real ones
@@ -43,82 +42,164 @@ function buildDirectedGraph(parent, node, graph, new_graph) {
     return node
 }
 
-// Algorithm following the Appendix of grapetree article
-function grapetree(vertices, trials) {
-    const gaps = []
-    gaps[0] = 2 * Math.PI / vertices.length
-    let best
-    for (let i = 0; i < trials; ++i) {
-        vertices.forEach(node => node.DCS = { radius: descendentsRadius(node), angle: descendentsAngle(node, gaps[i]) })
-        const angle = vertices.map(node => node.DCS.angle).reduce(max, -Infinity)
-        if (angle <= 2 * Math.PI && (!best || best.angle < angle))
-            best = { gap: gaps[i], angle, vertices: Object.assign({}, vertices) }
-        gaps[i + 1] = gaps[i] * 2 * Math.PI / angle
-    }
-    vertices = best.vertices
-    return best.gap
+function grapetree(nodes, linkScale) {
+	let maxRadius = 0.0, minGap = 0.0
+	nodes.forEach(node => {
+		if (!node.size) node.size = (node.children && node.children.length > 0 ? 0.01 : 1)
+		if (node.distance > maxRadius) maxRadius = node.distance
+		minGap += node.size
+	})
+	const minRadius = (maxRadius / linkScale) * 2 * Math.pow(nodes[nodes.length - 1].size, 0.5)
+	minGap = minRadius * Math.PI / minGap
+	for (let trials = 0; trials < 20; trials++) {
+		const gaps = []
+		let maxSpan
+		for (let i = 0; i < 11; i++) {
+			maxSpan = [[], 0]
+			for (let index = nodes.length - 1; index >= 0; index--)
+				maxSpan = descendentsRadius(nodes[index], minRadius, minGap, maxSpan, index)
+			if (Math.PI >= maxSpan[1]) {
+				gaps.push(minGap)
+				if (Math.PI < 1.05 * maxSpan[1]) break
+			}
+			if (i == 9) {
+				gaps.sort((r1, r2) => r2 - r1)
+				minGap = gaps[0]
+			} else if (i < 9)
+				minGap = (Math.PI / maxSpan[1]) * minGap
+		}
+		if (trials == 19) maxSpan[0] = [0]
+		maxSpan[0].forEach(index => setSpacing(nodes[index], minGap))
+		if (!maxSpan[0].length) break
+	}
+	return convertToCartesian(nodes, minRadius)
 }
 
-function max(u, v) {
-    return Math.max(u, v)
+function descendentsRadius(node, minRadius, minGap, maxSpan, index) {
+	if (node.spacing) return maxSpan
+	const nr = Math.pow(node.size, 0.5)
+	const span1 = [minRadius * nr, Math.asin(minGap * nr / minRadius)]
+	let radius = 0, angle = 0 // descending span
+	node.children.forEach(child => {
+		if (child.descendentsSpan) {
+			let selfRadius = minRadius * Math.sqrt(node.size) + child.distance
+			let span
+			if (Math.cos(Math.PI - child.descendentsSpan[1]) > child.descendentsSpan[0] / selfRadius) {
+				if (child.descendentsSpan[0] < selfRadius)
+					span = [selfRadius, Math.asin(child.descendentsSpan[0] / selfRadius)]
+			} else {
+				span = toPolar(toCartesian(child.descendentsSpan), [-selfRadius, 0])
+				span[0] = Math.max(span[0], selfRadius, child.descendentsSpan[0])
+			}
+			child.selfSpan = span
+			angle += span[1] + minGap / span[0]
+			if (span[0] > radius) radius = span[0]
+		}
+	})
+	const span2 = [radius, angle]
+	node.descendentsSpan = [Math.max(span1[0], span2[0]), span2[1]] //Math.max(span1[1], span2[1])]
+	if (span1[0] * span1[1] > node.descendentsSpan[0] * node.descendentsSpan[1])
+		node.descendentsSpan[1] = (span1[0] * span1[1]) / node.descendentsSpan[0]
+	node.descendentsAngle = span2[1]
+	if (node.descendentsSpan[1] > maxSpan[1])
+		maxSpan = [[index], node.descendentsSpan[1]]
+	else if (node.descendentsSpan[1] == maxSpan[1])
+		maxSpan[0].push(index)
+	if (node.descendentsSpan[1] > Math.PI) node.descendentsSpan[1] = Math.PI
+	return maxSpan
 }
 
-// Calculates radius of the direct descendents
-function childrenRadius(node) {
-    const radius1 = node.distance + 2 * node.circle_radius
-    if (!node.children.length)
-        return radius1
-    const radius2 = node.distance + node.circle_radius + descendentsRadius(node)
-    return Math.max(radius1, radius2)
+function convertToCartesian(nodes, minRadius) {
+	const coordinates = {}
+	nodes.forEach((node, index) => {
+		let initialAngle, gap
+		if (index > 0) {
+			initialAngle = -node.descendentsAngle
+			gap = 0
+		} else {
+			initialAngle = 0
+			gap = Math.max(0, (Math.PI - node.descendentsAngle) / node.children.length)
+			node.polar = [0, 0]
+			node.coordinates = [0, 0]
+		}
+		const selfRadial = minRadius * Math.sqrt(node.size)
+		coordinates[node.id] = node.coordinates
+		if (node.children) {
+			if (node.children.length > 2)
+				node.children.sort((c1, c2) => hashCode(c1.id) - hashCode(c2.id))
+			node.children.forEach(child => {
+				const minAngle = node.spacing / child.selfSpan[0]
+				child.polar = [child.distance + selfRadial, initialAngle + child.selfSpan[1] + minAngle + node.polar[1]]
+				initialAngle += (child.selfSpan[1] + minAngle + gap) * 2
+				child.coordinates = toCartesian(child.polar)
+				child.coordinates[0] += node.coordinates[0]
+				child.coordinates[1] += node.coordinates[1]
+			})
+		}
+		delete node.spacing, delete node.descendentsAngle, delete node.descendentsSpan, delete node.polar, delete node.coordinates
+	})
+	return coordinates
 }
 
-// Calculates radius of all the descendents of node
-function descendentsRadius(node) {
-    return node.children.length ? node.children.map(childrenRadius).reduce(max, -Infinity) : node.distance + 2 * node.circle_radius
+function setSpacing(node, minGap) {
+	node.spacing = minGap
+	if (node.children)
+		node.children.forEach(child => child.spacing || setSpacing(child, minGap))
 }
 
-// Calculates angle of direct descendents of node
-function childrenAngle(node, s) {
-    const angle1 = 2 * Math.asin(node.circle_radius * s / (node.distance + node.circle_radius))
-    if (!node.children.length)
-        return angle1
-    const radius = descendentsRadius(node)
-    const angle = descendentsAngle(node, s)
-    const arcsin = Math.asin(radius / (node.distance + node.circle_radius))    // Sometimes it's NaN, for example : 3/(1+1) 
-    const angle_poleMove = poleMove([radius, 0.5 * angle], [node.distance + node.circle_radius, Math.PI]).angle
-    const angle2 = 2 * (arcsin ? Math.max(arcsin, angle_poleMove) : angle_poleMove)
-    return Math.max(angle1, angle2)
+function hashCode(s) {
+	return s.split("").reduce((a, b) => ((a << 5) - a) + b.charCodeAt(0), 0);
 }
 
-// Calculates angle of all the descendents of node
-function descendentsAngle(node, s) {
-    if (!node.children.length)
-        return 2 * Math.asin(node.circle_radius * s / (node.distance + node.circle_radius))
-    const radius = s / descendentsRadius(node)
-    return node.children.reduce((sum, child) => sum + childrenAngle(child, s) + radius, 0)
+function toCartesian(coord, center) {
+	center = center || [0, 0]
+	const radius = coord[0], angle = coord[1]
+	return [radius * Math.cos(angle) + center[0], radius * Math.sin(angle) + center[1]]
 }
-
-function poleMove([rp, ap], [ro, ao]) {
-    const [xp, yp] = [rp * Math.cos(ap), rp * Math.sin(ap)]
-    const [xo, yo] = [ro * Math.cos(ao), ro * Math.sin(ao)]
-    const [x, y] = [xp - xo, yp - yo]
-    return { radius: Math.sqrt(x * x + y * y), angle: Math.atan(y / x) }
+function toPolar(coord, center) {
+	center = center || [0, 0]
+	const x = coord[0] - center[0], y = coord[1] - center[1]
+	return [Math.sqrt(x * x + y * y), Math.atan2(y, x)]
 }
 
 //////////////////////////////////////////////////////////////////////D3//////////////////////////////////////////////////////////////////////
 
+
 function createGrapeTree(graph) {
-    var svgContainer = d3.select("body")
-        .append("svg")
-        .attr("width", 500)
-        .attr("height", 500)
-    var circles = svgContainer
-        .selectAll("circle")
+
+    const width = 1000
+    const height = 1000
+
+    const svg = d3.select('body')
+        .append('svg')
+        .attr("width", width)
+        .attr("height", height)
+        .call(d3.zoom().on("zoom", () => svg.attr("transform", d3.event.transform))).append('g')
+
+    let link = svg.append('g')
+        .attr('class', 'links')
+        .selectAll('line')
+
+    let node = svg.append('g')
+        .attr('class', 'nodes')
+        .selectAll('circle')
+    const n = 1
+    link = link.data(graph.edges)
+        .enter()
+        .append('line')
+        .attr("x1", d => n * d.source.DCS.radius * Math.cos(d.source.DCS.angle) + width / 2)
+        .attr("y1", d => n * d.source.DCS.radius * Math.sin(d.source.DCS.angle) + height / 2)
+        .attr("x2", d => n * d.target.DCS.radius * Math.cos(d.target.DCS.angle) + width / 2)
+        .attr("y2", d => n * d.target.DCS.radius * Math.sin(d.target.DCS.angle) + height / 2)
+        .attr('stroke-width', 1)
+
+    let nodes = node
         .data(graph.vertices)
         .enter()
         .append("circle")
-        .attr("cx", d => d.DCS.radius * Math.cos(d.DCS.angle))
-        .attr("cy", d => d.DCS.radius * Math.sin(d.DCS.angle))
-        .attr("r", function (d) { return d.radius })
+        .attr("cx", d => (d.DCS.radius * n) * Math.cos(d.DCS.angle) + width / 2)
+        .attr("cy", d => (d.DCS.radius * n) * Math.sin(d.DCS.angle) + height / 2)
+        .attr("r", function (d) { return d.circle_radius })
         .style("fill", "green");
+
 }
