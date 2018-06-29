3566d658a3efc0e4a22173ba63ca5450fdc2d450
Luana Silva <luana.b.silva@hotmail.com>
2018-06-29 12:16:59 +0100

    .gitignore package-lock.json fix. Test input files rename. build.js cleaning (not tested).

--- Project/build.js
+++ Project/build.js
@@ -1,21 +1,14 @@
-var electronInstaller = require('electron-winstaller')
+'use strict'
 
-// In this case, we can use relative paths
-var settings = {
-    // Specify the folder where the built app is located
+const electronInstaller = require('electron-winstaller')
+
+const settings = {
     appDirectory: './PHYLOViZ-Electron-win32-x64',
-    // Specify the existing folder where 
     outputDirectory: './installers',
-    // The name of the Author of the app (the name of your company)
     authors: 'PHYLOViZ',
-    // The name of the executable of your built
     exe: './PHYLOViZ-Electron.exe'
 }
 
-resultPromise = electronInstaller.createWindowsInstaller(settings);
- 
-resultPromise.then(() => {
-    console.log("The installers of your application were succesfully created !");
-}, (e) => {
-    console.log(`Well, sometimes you are not so lucky: ${e.message}`)
-})
+electronInstaller.createWindowsInstaller(settings)
+    .then(() => console.log("The installers of your application were succesfully created!"))
+    .catch(e => console.log(`Well, sometimes you are not so lucky: ${e.message}`))
