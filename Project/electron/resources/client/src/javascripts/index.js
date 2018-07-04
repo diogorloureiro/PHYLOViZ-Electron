68abc26c84189647110b43453a07ed399e252047
Luana Silva <luana.b.silva@hotmail.com>
2018-07-04 07:06:58 +0100

    Remove graph-handler.

--- Project/electron/resources/client/src/javascripts/index.js
+++ Project/electron/resources/client/src/javascripts/index.js
@@ -2,7 +2,6 @@
 
 import * as d3 from 'd3'
 
-import { flatten, direct, ancillary } from './graph-handler'
 import renders from './renders'
 
 let link, node, simulation, conf
@@ -69,6 +68,49 @@ function init(algorithm) {
         }
     }
 
+    function flatten(node, nodes = []) {
+        nodes.push(node)
+        if (node.children)
+            node.children.forEach(child => flatten(child, nodes))
+        return nodes
+    }
+    
+    function direct(graph) {
+        const { vertices, edges } = graph
+        const root = vertices.shift()
+        root.distance = 0
+        const neighbours = [root], visited = []
+        while (neighbours.length) {
+            const neighbour = neighbours.shift()
+            const id = neighbour.id
+            neighbour.children = []
+            edges.forEach((edge, index) => {
+                let { source, target, distance } = edge
+                if (id === target) {
+                    const tmp = source
+                    source = target
+                    target = tmp
+                }
+                if (id === source) {
+                    visited.push({ source, target, distance })
+                    delete edges[index]
+                    const child = vertices.find(vertex => vertex.id === target)
+                    child.distance = distance
+                    neighbour.children.push(child)
+                    neighbours.push(child)
+                }
+            })
+        }
+        return { root, edges: visited }
+    }
+    
+    function ancillary(graph, ancillary = [], minimum = 5) {
+        graph.vertices.forEach(vertex => {
+            vertex.ancillary = ancillary.filter(row => vertex.id === row.st)
+            vertex.size = vertex.ancillary.length + minimum
+        })
+    }
+    
     return {
         flatten,
         direct,
