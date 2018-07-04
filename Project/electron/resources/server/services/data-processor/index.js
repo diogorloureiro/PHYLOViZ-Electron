2fc30e2bbc30b17411cd02a3eb745d3e4d475362
Luana Silva <luana.b.silva@hotmail.com>
2018-07-04 06:52:22 +0100

    Data processor invalid parameters handling.

--- Project/electron/resources/server/services/data-processor/index.js
+++ Project/electron/resources/server/services/data-processor/index.js
@@ -4,11 +4,13 @@ const processors = require('./processors')
 const comparators = require('./comparators')
 const algorithms = require('./algorithms')
 const modes = require('./modes')
+const RequestError = require('../../RequestError')
 
 function init(mode = 'promises') {
     const process = modes[mode](processors, comparators, algorithms)
     return function (processor = 'goeburst', comparator = 'goeburst', algorithm = 'boruvka', profiles, lvs) {
         return process(processor, comparator, algorithm, profiles, lvs)
+            .catch(() => { throw new RequestError('Invalid parameters', 400) })
     }
 }
 
