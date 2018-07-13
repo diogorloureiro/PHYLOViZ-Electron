801727ea9ac0fb9c85993fdeddc498a453de913e
JupitersMight <leonardo_duarte_rodrigues@hotmail.com>
2018-07-13 22:49:03 +0100

    reorganized

--- Project/electron/resources/client/src/javascripts/renders/radial.js
+++ Project/electron/resources/client/src/javascripts/renders/radial.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import renderStaticTree from './treebuild'
+import * as d3 from 'd3'
 
 const defaultDistanceMultiplier = 100
 const defaultZeroDistanceValue = 10
@@ -40,6 +40,15 @@ function leafcount(node) {
 }
 
 function render(graph, conf, onNodeClick) {
+
+    let nodes = d3.selectAll('.node')
+    if(nodes)
+        nodes.remove()
+    
+    let links = d3.selectAll('.link')
+    if(links)
+        links.remove()
+
     radial(graph.vertices[0])
 
     graph.vertices.forEach(vertex => {
@@ -52,7 +61,53 @@ function render(graph, conf, onNodeClick) {
         })
     })
 
-    renderStaticTree(graph,conf,onNodeClick)
+    conf.link = conf.link
+        .data(graph.edges.filter(edge => graph.vertices.find(vertex => vertex.id === edge.target.id)))
+
+    let edgeEnter = conf.link
+        .enter()
+        .append('g')
+        .attr('class', 'link')
+        .attr('transform', 'rotate(0)')
+
+    edgeEnter
+        .append('line')
+        .attr('x1', d => d.source.x + conf.width / 2)
+        .attr('y1', d => d.source.y + conf.height / 2)
+        .attr('x2', d => d.target.x + conf.width / 2)
+        .attr('y2', d => d.target.y + conf.height / 2)
+        .style('stroke', '#000000')
+        .attr('stroke-width', 1)
+
+    nodes = conf.node
+        .data(graph.vertices)
+
+    let nodeEnter = nodes
+        .enter()
+        .append('g')
+        .attr('class', 'node')
+        .attr('transform', 'rotate(0)')
+
+    nodeEnter
+        .on('click', onNodeClick)
+        .append('circle')
+        .attr('id', d => 'node' + d.id)
+        .attr('cx', d => d.x + conf.width / 2)
+        .attr('cy', d => d.y + conf.height / 2)
+        .attr('r', d => d.size)
+        .attr('fill', d => d._children && d._children.length > 0 ? 'orange' : '#00549f')
+
+    nodeEnter
+        .append('text')
+        .attr('id', d => 'text' + d.id)
+        .style('visibility', 'visible')
+        .attr('dx', -1)
+        .attr('dy', 0)
+        .attr('x', d => d.x + conf.width / 2)
+        .attr('y', d => d.y + conf.height / 2)
+        .text(d => d.id)
+        .style('font-size', d => (d.size / (d.id + '').length) + 'px')
+        .attr('fill', 'white')
 }
 
 export default render
