c19261109d2bbd0ce341542a095b81b5a9452e70
JupitersMight <leonardo_duarte_rodrigues@hotmail.com>
2018-07-13 21:27:01 +0100

    reorganized structure of views, now any view can create a staticTree just need to import the function and pass the parameters

--- Project/electron/resources/client/src/javascripts/renders/grapetree.js
+++ Project/electron/resources/client/src/javascripts/renders/grapetree.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import * as d3 from 'd3'
+import renderStaticTree from './treebuild'
 
 function grapetree(nodes, linkScale = 0.3, trials = 10) {
     let maxRadius = 0.0, minWedge = 0.0
@@ -85,7 +85,8 @@ function convertToCartesian(nodes, minRadius) {
             initialAngle = 0
             wedge = Math.max(0, (Math.PI - node.descendentsAngle) / node.children.length)
             node.polar = [0, 0]
-            node.coordinates = [0, 0]
+            node.x = 0
+            node.y = 0
         }
         const selfRadial = minRadius * Math.sqrt(node.size)
         if (node.children !== []) {
@@ -95,9 +96,11 @@ function convertToCartesian(nodes, minRadius) {
                 const minAngle = node.spacing / child.selfSpan[0]
                 child.polar = [child.distance + selfRadial, initialAngle + child.selfSpan[1] + minAngle + node.polar[1]]
                 initialAngle += (child.selfSpan[1] + minAngle + wedge) * 2
-                child.coordinates = toCartesian(child.polar)
-                child.coordinates[0] += node.coordinates[0]
-                child.coordinates[1] += node.coordinates[1]
+                const coordinates = toCartesian(child.polar)
+                child.x = coordinates[0]
+                child.y = coordinates[1]
+                child.x += node.x
+                child.y += node.y
             })
         }
         delete node.spacing, delete node.descendentsAngle, delete node.descendentsSpan, delete node.polar
@@ -120,75 +123,8 @@ function toPolar(coord, center = [0, 0]) {
 }
 
 function render(graph, conf, onNodeClick) {
-
-    let nodes = d3.selectAll('.node')
-    if(nodes)
-        nodes.remove()
-    
-    let links = d3.selectAll('.link')
-    if(links)
-        links.remove()
-
     grapetree(graph.vertices)
-
-    graph.vertices.forEach(vertex => {
-        graph.edges.forEach(edge => {
-            const id = vertex.id
-            const [x, y] = vertex.coordinates
-            if (id === edge.source || id === edge.source.id)
-                edge.source = { x, y, id }
-            else if (id === edge.target || id === edge.target.id)
-                edge.target = { x, y, id }
-        })
-    })
-
-    conf.link = conf.link
-        .data(graph.edges.filter(edge => graph.vertices.find(vertex => vertex.id === edge.target.id)))
-
-    let edgeEnter = conf.link
-        .enter()
-        .append('g')
-        .attr('class', 'link')
-        .attr('transform', d => 'rotate(0)')
-
-    edgeEnter
-        .append('line')
-        .attr('x1', d => d.source.x + conf.width / 2)
-        .attr('y1', d => d.source.y + conf.height / 2)
-        .attr('x2', d => d.target.x + conf.width / 2)
-        .attr('y2', d => d.target.y + conf.height / 2)
-        .style('stroke', '#000000')
-        .attr('stroke-width', 1)
-
-    nodes = conf.node
-        .data(graph.vertices)
-
-    let elemEnter = nodes
-        .enter()
-        .append('g')
-        .attr('class', 'node')
-        .attr('transform', 'rotate(0)')
-
-    elemEnter
-        .on('click', onNodeClick)
-        .append('circle')
-        .attr('id', d => 'node' + d.id)
-        .attr('cx', d => d.coordinates[0] + conf.width / 2)
-        .attr('cy', d => d.coordinates[1] + conf.height / 2)
-        .attr('r', d => d.size)
-        .attr('fill', d => d._children && d._children.length > 0 ? 'orange' : '#00549f')
-
-    elemEnter
-        .append('text')
-        .attr('id', d => 'text' + d.id)
-        .style('visibility', 'visible')
-        .attr('dx', -1)
-        .attr('dy', 1)
-        .attr('x', d => d.coordinates[0] + conf.width / 2)
-        .attr('y', d => d.coordinates[1] + conf.height / 2)
-        .text(d => d.id)
-        .style('font-size', d => (d.size / (d.id + '').length + 1) + 'px')
-        .attr('fill', 'white')
+    renderStaticTree(graph,conf,onNodeClick)
 }
 
 export default render
---
+++ Project/electron/resources/client/src/javascripts/renders/treebuild/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+import staticTree from './staticTree'
+
+export default staticTree
